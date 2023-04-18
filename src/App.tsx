6c417dfb587a6702e0fa9d6b36a773a3a9f915c3
Joey <joeyvandervalk@gmail.com>
2023-04-18 13:11:40 +0200

    Installed tailwind, daisyui and supabase.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <button className="btn btn-primary">Button</button>
+    </div>
+  );
+};
 
 export default App;
