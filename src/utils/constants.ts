888b29aac11e91e017d95dd1bd05c33f30f2e6a6
Joey <joeyvandervalk@gmail.com>
2023-06-01 20:43:32 +0200

    aaaa

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,18 @@
+// Routing
+export const ROOT_ROUTE = "/";
+export const ADDRESS_SEARCH_ROUTE = "/address_search";
+export const IMAGES_ROUTE = "/images";
+
+// Storage
+export const BUILDING_TABLE_NAME = "buildings";
+export const BUILDING_IMAGE_BUCKET = "building-images";
+
+// Regex
 export const EMAIL_REGEX =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 export const POSTAL_CODE_REGEX = /^[1-9][0-9]{3} ?(?!sa|sd|ss)[a-z]{2}$/i;
 
+// Forms
 export const INITIAL_SIGN_IN_FORM_DATA = {
   email: "",
   password: "",
@@ -14,10 +25,3 @@ export const DEFAULT_BUILDING_FORM_DATA = {
   lat: 0,
   imageUrl: "",
 };
-
-export const BUILDING_TABLE_NAME = "buildings";
-export const BUILDING_IMAGE_BUCKET = "building-images";
-
-export const ROOT_ROUTE = "/";
-export const ADDRESS_SEARCH_ROUTE = "/address_search";
-export const IMAGES_ROUTE = "/images";
