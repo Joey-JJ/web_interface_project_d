5cc5ca02167853bccb8bec4ad2de85c15998a5d5
Joey <joeyvandervalk@gmail.com>
2023-06-01 20:46:04 +0200

    x

--- src/index.tsx
+++ src/index.tsx
@@ -34,11 +34,7 @@ const router = createBrowserRouter([
   },
 ]);
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-
-root.render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
